8d6c34ce4d061d8c390ea662930417f70918e6b7
yoid2000 <paul@francis.com>
2024-04-04 12:58:34 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="syndiffix-tools",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A variety of tools for managing SynDiffix synthetic data.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Open Diffix",
+    author_email="hello@open-diffix.org",
+    url="https://github.com/diffix/syndiffix-tools",
+    install_requires=['syndiffix'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
