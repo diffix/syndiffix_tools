62402f687b7c587fabb95316c3a8d3d3a1638841
yoid2000 <paul@francis.com>
2024-05-12 14:22:35 +0200

    bump version number to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="syndiffix_tools",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     description="A variety of tools for managing SynDiffix synthetic data.",
     long_description=open("README.md").read(),
